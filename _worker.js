b829f24a69ff092eea29484ad578c2435c8b2de7
koureni <80780771+koureni@users.noreply.github.com>
2024-07-14 19:45:40 +0800

    Update _worker.js
    更换UUID

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '01708a9a-4e3e-4be1-93bb-852fa88f852d';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
